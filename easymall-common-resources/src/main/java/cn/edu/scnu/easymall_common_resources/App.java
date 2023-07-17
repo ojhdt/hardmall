8ab24a892aca527e55fabf963b3d18f8da1491cb
Ojhdt <ojhdtmail@gmail.com>
2023-07-17 16:07:58 +0800

    2

---
+++ easymall-common-resources/src/main/java/cn/edu/scnu/easymall_common_resources/App.java
@@ -0,0 +1,13 @@
+package cn.edu.scnu.easymall_common_resources;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
