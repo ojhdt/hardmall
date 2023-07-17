48cf998eb3a67db9c6752c875d314a063a6a14a2
Ojhdt <ojhdtmail@gmail.com>
2023-07-17 17:11:28 +0800

    server01

---
+++ eureka-server/src/main/java/cn/edu/scnu/StarterEurekaServer01.java
@@ -0,0 +1,14 @@
+package cn.edu.scnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer 
+public class StarterEurekaServer01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterEurekaServer01.class, args);
+	}
+}
